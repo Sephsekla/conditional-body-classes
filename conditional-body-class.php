42b6a65600f2c55cfdcc302a16ecf266c8021241
Joe Bailey-Roberts <dev@joebr.io>
2020-03-21 16:25:31 +0000

    Add php minimum

---
+++ conditional-body-class.php
@@ -0,0 +1,46 @@
+<?php
+/*
+	Plugin Name: Conditional Body Class
+	Plugin URI: https://joebr.io
+	Description: Add body classes based on conditional factors on your site
+	Version: 0.1.0
+	Author: JBR Digital
+	Author URI: https://www.never5.com
+	Requires at least: 5.3.0
+	Tested up to: 5.3.1
+	Text Domain: cbc
+
+	License: GPL v3
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+
+// Define DLM FILE
+define( 'CBC_PLUGIN_PATH', plugin_dir_path(__FILE__) );
+
+/**
+ * Check we have a PHP version high enough to support namespaces
+ */
+if ( version_compare( PHP_VERSION, '5.3.0' ) >= 0 ) {
+	require_once CBC_PLUGIN_PATH.'/inc/options.php';
+}else {
+	require_once CBC_PLUGIN_PATH.'/inc/php-version.php';
+}
+
---
+++ inc/php-version.php
@@ -0,0 +1,20 @@
+ <?php
+/**
+ * PHP minimum version
+ *
+ * Displays an error message when the minimum php version is not met.
+ *
+ * @link URL
+ *
+ * @package cbc
+ * @since 0.1.0
+ */
+
+
+function cbc_php_requirement_notice() {
+    $class = 'notice notice-error';
+    $message = __( 'Conditional Body Class requires a minimum PHP version of 5.3. For more information visit <a href="https://wordpress.org/support/update-php/" target="_blank">WordPress Support</a>', 'cbc' );
+ 
+    printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) ); 
+}
+add_action( 'admin_notices', 'cbc_php_requirement_notice' );
