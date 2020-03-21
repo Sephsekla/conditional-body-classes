91da3c8477faef13951c6cd7b5998044073b939d
Joe Bailey-Roberts <dev@joebr.io>
2020-03-21 17:40:18 +0000

    Move from class to namespace organisation

--- inc/options.php
+++ inc/options.php
@@ -10,51 +10,37 @@
 
  namespace cbc\options;
 
- /**
-  * Class for registering a new settings page under Settings.
-  */
-class options_page
-{
- 
-    /**
-     * Constructor.
-     */
-    function __construct()
-    {
-        add_action('admin_menu', array( $this, 'admin_menu' ));
-        add_action('admin_menu', array( $this, 'create_settings_section' ));
-    }
+
+add_action('admin_menu', __NAMESPACE__.'\admin_menu');
+add_action('admin_menu', __NAMESPACE__.'\create_settings_section');
  
     /**
      * Registers a new settings page under Settings.
      */
-    function admin_menu()
-    {
-        add_options_page(
-            __('Conditional Body Classes', 'cbc'),
-            __('Body Classes', 'cbc'),
-            'manage_options',
-            'cbc',
-            array(
-                $this,
-                'settings_page'
-            )
-        );
+function admin_menu()
+{
+    add_options_page(
+        __('Conditional Body Classes', 'cbc'),
+        __('Body Classes', 'cbc'),
+        'manage_options',
+        'cbc',
+        __NAMESPACE__.'\settings_page'
+    );
 
-    }
+}
 
     /**
      * Settings page display callback.
      */
-    function settings_page()
-    {
+function settings_page()
+{
 
-        // check user capabilities
-        if (! current_user_can('manage_options') ) {
-            return;
-        }
+    // check user capabilities
+    if (! current_user_can('manage_options') ) {
+        return;
+    }
     
-        ?>
+    ?>
     <div class="wrap">
     <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
     <form action="options-general.php" method="post">
@@ -70,28 +56,27 @@ class options_page
     </form>
     </div>
         <?php
-    }
-
-    function create_settings_section()
-    {
-
-        register_setting( 'cbc_classes', 'cbc' );
-
-
-        add_settings_section(
-            'cbc_section',
-            __('Body Classes.', 'cbc'),
-            array($this,'cbc_section_callback'),
-            'cbc'
-        );
-    }
-
-    function cbc_section_callback()
-    {
-        echo  __('Intro', 'cbc');
-    }
-
- 
 }
+
+function create_settings_section()
+{
+
+    register_setting('cbc_classes', 'cbc');
+
+
+
+
+    add_settings_section(
+        'cbc_section',
+        __('Body Classes.', 'cbc'),
+        __NAMESPACE__.'\cbc_section_callback',
+        'cbc'
+    );
+}
+
+function cbc_section_callback()
+{
+    echo  __('Intro', 'cbc');
+}
+
  
-new options_page;
