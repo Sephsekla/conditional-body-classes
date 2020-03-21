beed7cc3861a1b41711cdda3d9aa1aae39c06c35
Joe Bailey-Roberts <dev@joebr.io>
2020-03-21 17:11:55 +0000

    Add settings page

--- inc/options.php
+++ inc/options.php
@@ -4,11 +4,91 @@
  *
  * Create options page for conditional classes
  *
- *
  * @package cbc
- * @since 0.3.0
+ * @since   0.3.0
  */
 
  namespace cbc\options;
 
+ /**
+  * Class for registering a new settings page under Settings.
+  */
+class options_page
+{
  
+    /**
+     * Constructor.
+     */
+    function __construct()
+    {
+        add_action('admin_menu', array( $this, 'admin_menu' ));
+        add_action('admin_menu', array( $this, 'create_settings_section' ));
+    }
+ 
+    /**
+     * Registers a new settings page under Settings.
+     */
+    function admin_menu()
+    {
+        add_options_page(
+            __('Conditional Body Classes', 'cbc'),
+            __('Body Classes', 'cbc'),
+            'manage_options',
+            'cbc',
+            array(
+                $this,
+                'settings_page'
+            )
+        );
+
+    }
+
+    /**
+     * Settings page display callback.
+     */
+    function settings_page()
+    {
+
+        // check user capabilities
+        if (! current_user_can('manage_options') ) {
+            return;
+        }
+    
+        ?>
+    <div class="wrap">
+    <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+    <form action="options-general.php" method="post">
+        <?php
+        // output security fields for the registered setting "cbc"
+        settings_fields('cbc');
+        // output setting sections and their fields
+        // (sections are registered for "cbc", each field is registered to a specific section)
+        do_settings_sections('cbc');
+        // output save settings button
+        submit_button('Save Settings');
+        ?>
+    </form>
+    </div>
+        <?php
+    }
+
+    function create_settings_section()
+    {
+
+
+        add_settings_section(
+            'cbc_section',
+            __('Body Classes.', 'cbc'),
+            array($this,'cbc_section_callback'),
+            'cbc'
+        );
+    }
+
+    function cbc_section_callback()
+    {
+        echo  __('Intro', 'cbc');
+    }
+ 
+}
+ 
+new options_page;
