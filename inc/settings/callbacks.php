6d470c311bbb9f93d5c9bb825cc9297d5e47d1a4
Joe Bailey-Roberts <dev@joebr.io>
2020-03-25 10:09:46 +0000

    Separate callbacks into different file

---
+++ inc/settings/callbacks.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace cbc\settings;
+
+use cbc\options as options;
+use cbc\helpers as helpers;
+
+function cbc_section_classes_cb()
+{
+    echo 'SECTION CALLBACk';
+}
+
+function cbc_field_classes_cb($args)
+{
+    $options = get_option('cbc_options');
+
+    $classes = $options['cbc_field_classes'];
+
+    echo '<pre>';
+    print_r($options);
+    echo '</pre>';
+    // output the field
+    ?>
+
+
+<fieldset id="<?php echo esc_attr($args['label_for']); ?>"
+    data-custom="<?php echo esc_attr($args['cbc_custom_data']); ?>">
+    <p class="description">
+        <?php esc_html_e('Lorem ipsum here\s a description', 'cbc'); ?>
+    </p>
+
+    <?php
+
+    $i = 0;
+
+    $operators = [
+    ['Page', 'page'],
+    ['Post Type', 'post_type'],
+
+    ];
+
+    do{
+
+        ?>
+
+    <input type="text" name="cbc_options[<?php echo esc_attr($args['label_for']); ?>][<?php echo $i ?>][classes]"
+        value="<?php echo $classes[$i]['classes']?>">
+    <select name="cbc_options[<?php echo esc_attr($args['label_for']); ?>][<?php echo $i ?>][operator]">
+
+        <?php foreach($operators as $operator){
+            ?>
+        <option value="<?php echo $operator[1] ?>"
+            <?php echo selected($operator[1] === $options[ $args['label_for'] ][$i]['operator'], true, false); ?>>
+            <?php echo $operator[0] ?></option>
+            <?php
+        }
+
+        ?>
+
+
+    </select>
+    <input type="text" name="cbc_options[<?php echo esc_attr($args['label_for']); ?>][<?php echo $i ?>][conditions]"
+        value="<?php echo $classes[$i]['conditions'] ?>">
+
+
+    <br>
+
+        <?php
+
+        $i++;
+
+    }
+    while($i < count($classes));
+
+    ?>
+
+</fieldset>
+    <?php
+}
+
+function cbc_field_permissions_cb($args)
+{
+    $options = get_option('cbc_options');
+
+
+    $roles = helpers\get_all_roles();
+  
+
+    // output the field
+    ?>
+
+
+<fieldset id="<?php echo esc_attr($args['label_for']); ?>"
+    data-custom="<?php echo esc_attr($args['cbc_custom_data']); ?>">
+    <p class="description">
+        <?php esc_html_e('Select which roles can access Conditional Body Classes', 'cbc'); ?>
+    </p>
+
+    <?php foreach($roles as $key=>$role){
+
+
+        if ('administrator' === $key) {
+
+            ?>
+
+    <input type="checkbox" disabled name="cbc_options[<?php echo esc_attr($args['label_for']); ?>][]"
+        value="<?php echo $key ?>"
+            <?php echo isset($options[ $args['label_for'] ]) ? ( checked(in_array($key, $options[ $args['label_for'] ]), true, false) ) : ( '' ); ?>><?php echo $role['name'] ?><br>
+
+    <input type="hidden" name="cbc_options[<?php echo esc_attr($args['label_for']); ?>][]" value="<?php echo $key ?>">
+
+            <?php
+
+        }
+
+        else{
+
+            ?>
+
+
+    <input type="checkbox" <?php echo ('administrator' === $key) ? 'disabled' : '' ?>
+        name="cbc_options[<?php echo esc_attr($args['label_for']); ?>][]" value="<?php echo $key ?>"
+            <?php echo isset($options[ $args['label_for'] ]) ? ( checked(in_array($key, $options[ $args['label_for'] ]), true, false) ) : ( '' ); ?>><?php echo $role['name'] ?><br>
+
+
+
+            <?php
+
+        }
+
+    }
+    ?>
+</fieldset>
+    <?php
+}
+    
+
